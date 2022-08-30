d607ad580aba3e7d698a041e5f6f07a2e0f4d146
kkaser <%Uh07^xaJZ0M>
2022-08-30 18:35:42 -0400

    Base application

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Lab1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(29, 32);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Line";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(132, 32);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Rectangle";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.BackColor = System.Drawing.Color.Red;
+			this.trackBar1.Location = new System.Drawing.Point(325, 32);
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(104, 45);
+			this.trackBar1.TabIndex = 2;
+			this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.BackColor = System.Drawing.Color.Green;
+			this.trackBar2.Location = new System.Drawing.Point(472, 32);
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(104, 45);
+			this.trackBar2.TabIndex = 3;
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.BackColor = System.Drawing.Color.Blue;
+			this.trackBar3.Location = new System.Drawing.Point(622, 32);
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(104, 45);
+			this.trackBar3.TabIndex = 5;
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+			this.panel1.Location = new System.Drawing.Point(29, 71);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(757, 374);
+			this.panel1.TabIndex = 6;
+			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+			this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(226, 32);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "Ellipse";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(798, 457);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.trackBar3);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+	// Contains main logic of application.
+	public partial class Form1 : Form
+	{
+		// Creates starting state of application GUI.
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// Select line class and create line on mouse clicks inside panel.
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Console.WriteLine("Hello");
+		}
+
+		// Gradient of Red to add to color written to panel.
+		private void trackBar1_ValueChanged(object sender, EventArgs e)
+		{
+			Console.WriteLine(trackBar1.Value);
+		}
+		
+		// To hold the X and Y points recorded on first click inside panel.
+		int startX, startY;
+
+		private void panel1_MouseDown(object sender, MouseEventArgs e)
+		{
+			// Record X and Y point to start creating shape from.
+			startX = e.X;
+			startY = e.Y;
+		}
+
+		private void panel1_MouseUp(object sender, MouseEventArgs e)
+		{
+			// Object to draw a new shape inside the panel.
+			Graphics g = panel1.CreateGraphics();
+
+			Pen pen = new Pen(Color.Red);
+			g.DrawLine(pen, startX, startY, e.X, e.Y);
+		}
+
+		private void panel1_MouseMove(object sender, MouseEventArgs e)
+		{
+			
+		}
+	}
+}
