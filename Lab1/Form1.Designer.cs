02052ad93b135bf79fb437b38f2546e69d07820b
Anthony <40270179+Imme21@users.noreply.github.com>
2022-08-30 18:57:36 -0400

    Updated Buttons

--- Lab1/Form1.Designer.cs
+++ Lab1/Form1.Designer.cs
@@ -29,116 +29,124 @@ namespace Lab1
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.trackBar1 = new System.Windows.Forms.TrackBar();
-			this.trackBar2 = new System.Windows.Forms.TrackBar();
-			this.trackBar3 = new System.Windows.Forms.TrackBar();
-			this.panel1 = new System.Windows.Forms.Panel();
-			this.button3 = new System.Windows.Forms.Button();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
-			this.SuspendLayout();
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(29, 32);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "Line";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(132, 32);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "Rectangle";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// trackBar1
-			// 
-			this.trackBar1.BackColor = System.Drawing.Color.Red;
-			this.trackBar1.Location = new System.Drawing.Point(325, 32);
-			this.trackBar1.Name = "trackBar1";
-			this.trackBar1.Size = new System.Drawing.Size(104, 45);
-			this.trackBar1.TabIndex = 2;
-			this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
-			// 
-			// trackBar2
-			// 
-			this.trackBar2.BackColor = System.Drawing.Color.Green;
-			this.trackBar2.Location = new System.Drawing.Point(472, 32);
-			this.trackBar2.Name = "trackBar2";
-			this.trackBar2.Size = new System.Drawing.Size(104, 45);
-			this.trackBar2.TabIndex = 3;
-			// 
-			// trackBar3
-			// 
-			this.trackBar3.BackColor = System.Drawing.Color.Blue;
-			this.trackBar3.Location = new System.Drawing.Point(622, 32);
-			this.trackBar3.Name = "trackBar3";
-			this.trackBar3.Size = new System.Drawing.Size(104, 45);
-			this.trackBar3.TabIndex = 5;
-			// 
-			// panel1
-			// 
-			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.LineButton = new System.Windows.Forms.Button();
+            this.RecButton = new System.Windows.Forms.Button();
+            this.RedBar = new System.Windows.Forms.TrackBar();
+            this.GreenBar = new System.Windows.Forms.TrackBar();
+            this.BlueBar = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EllipButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.RedBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LineButton
+            // 
+            this.LineButton.Location = new System.Drawing.Point(39, 39);
+            this.LineButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LineButton.Name = "LineButton";
+            this.LineButton.Size = new System.Drawing.Size(100, 28);
+            this.LineButton.TabIndex = 0;
+            this.LineButton.Text = "Line";
+            this.LineButton.UseVisualStyleBackColor = true;
+            this.LineButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // RecButton
+            // 
+            this.RecButton.Location = new System.Drawing.Point(170, 39);
+            this.RecButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RecButton.Name = "RecButton";
+            this.RecButton.Size = new System.Drawing.Size(100, 28);
+            this.RecButton.TabIndex = 1;
+            this.RecButton.Text = "Rectangle";
+            this.RecButton.UseVisualStyleBackColor = true;
+            // 
+            // RedBar
+            // 
+            this.RedBar.BackColor = System.Drawing.Color.Red;
+            this.RedBar.Location = new System.Drawing.Point(433, 39);
+            this.RedBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RedBar.Name = "RedBar";
+            this.RedBar.Size = new System.Drawing.Size(139, 56);
+            this.RedBar.TabIndex = 2;
+            this.RedBar.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // GreenBar
+            // 
+            this.GreenBar.BackColor = System.Drawing.Color.Green;
+            this.GreenBar.Location = new System.Drawing.Point(629, 39);
+            this.GreenBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GreenBar.Name = "GreenBar";
+            this.GreenBar.Size = new System.Drawing.Size(139, 56);
+            this.GreenBar.TabIndex = 3;
+            // 
+            // BlueBar
+            // 
+            this.BlueBar.BackColor = System.Drawing.Color.Blue;
+            this.BlueBar.Location = new System.Drawing.Point(829, 39);
+            this.BlueBar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BlueBar.Name = "BlueBar";
+            this.BlueBar.Size = new System.Drawing.Size(139, 56);
+            this.BlueBar.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-			this.panel1.Location = new System.Drawing.Point(29, 71);
-			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(757, 374);
-			this.panel1.TabIndex = 6;
-			this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
-			this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
-			this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(226, 32);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(75, 23);
-			this.button3.TabIndex = 7;
-			this.button3.Text = "Ellipse";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(798, 457);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.panel1);
-			this.Controls.Add(this.trackBar3);
-			this.Controls.Add(this.trackBar2);
-			this.Controls.Add(this.trackBar1);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Location = new System.Drawing.Point(39, 87);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1009, 460);
+            this.panel1.TabIndex = 6;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // EllipButton
+            // 
+            this.EllipButton.Location = new System.Drawing.Point(301, 39);
+            this.EllipButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EllipButton.Name = "EllipButton";
+            this.EllipButton.Size = new System.Drawing.Size(100, 28);
+            this.EllipButton.TabIndex = 7;
+            this.EllipButton.Text = "Ellipse";
+            this.EllipButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 562);
+            this.Controls.Add(this.EllipButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BlueBar);
+            this.Controls.Add(this.GreenBar);
+            this.Controls.Add(this.RedBar);
+            this.Controls.Add(this.RecButton);
+            this.Controls.Add(this.LineButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.RedBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BlueBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.TrackBar trackBar1;
-		private System.Windows.Forms.TrackBar trackBar2;
-		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.Button LineButton;
+		private System.Windows.Forms.Button RecButton;
+		private System.Windows.Forms.TrackBar RedBar;
+		private System.Windows.Forms.TrackBar GreenBar;
+		private System.Windows.Forms.TrackBar BlueBar;
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button EllipButton;
 	}
 }
 
